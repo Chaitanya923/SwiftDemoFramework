cb0043e41244f941ba09e492b3ecc336170400e8
Chaitanya Manjrekar <chaitanya.manjrekar@neosoftmail.com>
2022-05-20 04:07:19 -0700

    Finished 1

---
+++ DemoSwiftFramework/DemoSwiftFramework/DemoSwiftFramework.h
@@ -0,0 +1,18 @@
+//
+//  DemoSwiftFramework.h
+//  DemoSwiftFramework
+//
+//  Created by neosoft on 20/05/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DemoSwiftFramework.
+FOUNDATION_EXPORT double DemoSwiftFrameworkVersionNumber;
+
+//! Project version string for DemoSwiftFramework.
+FOUNDATION_EXPORT const unsigned char DemoSwiftFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DemoSwiftFramework/PublicHeader.h>
+
+
---
+++ DemoSwiftFramework/DemoSwiftFramework/Service.swift
@@ -0,0 +1,18 @@
+//
+//  Service.swift
+//  DemoSwiftFramework
+//
+//  Created by neosoft on 20/05/22.
+//
+
+import Foundation
+
+public class Service{
+    
+    public init(){}
+    
+    public func doSomething() -> String{
+        return "Random String Sent"
+    }
+    
+}
